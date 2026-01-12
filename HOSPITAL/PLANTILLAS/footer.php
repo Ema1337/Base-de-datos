61aa2e6f1afb72bb70c2280c44309e311dc874eb
Ema1337 <ehernandezh2010@alumno.ipn.mx>
2026-01-12 09:08:06 -0600

    Add footer to footer.php with copyright and links

---
+++ HOSPITAL/PLANTILLAS/footer.php
@@ -0,0 +1,16 @@
+</main>
+    
+    <footer class="bg-dark text-white p-4 mt-5">
+        <div class="container text-center">
+            <p>&copy; <?php echo date("Y"); ?> HOSPITAL SAN ANGEL INN. Todos los derechos reservados.</p>
+            <p>
+                <a href="#" class="text-white me-2">Facebook</a> |
+                <a href="#" class="text-white me-2">Twitter</a> |
+                <a href="#" class="text-white">Instagram</a>
+            </p>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
